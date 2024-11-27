a8553fa54ebb94204616ab057178d52b99cde6b4
BigPhu <bigphu2005@gmail.com>
2024-11-27 12:17:35 +0700

    Add compilation instruction

--- src/program.cpp
+++ src/program.cpp
@@ -14,6 +14,9 @@ namespace fs = std::filesystem;
 // TODO Add new functions and classes to run UGraphDemo
 // #include "demo/graph/UGraphDemo.h" 
 
+// NOTE: Để chạy demo, mở terminal chạy WSL, sau đó gõ vào lệnh "./compilation-command.sh" và đợi G++ compile. 
+//       Gõ "./program" để chạy demo sau khi đã compile xong
+
 int main(int argc, char** argv) {
 
     runAllDGraphDemos();
